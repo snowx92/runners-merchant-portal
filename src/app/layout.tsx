7062584b5bbdf9e9d7f87c360985c65e8b90d07d
Mohamed Elshreef <jonsnowelshreef92@gmail.com>
2026-01-06 04:44:03 +0200

    added fav icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/navLogo.png" type="image/png" />
+      </head>
       <body className={outfit.className}>{children}</body>
     </html>
   );
